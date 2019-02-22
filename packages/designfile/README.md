7f5e3f11fd8f0734697ce23a279e90b311b83008
Roberto Dip <rob@haiku.ai>
2019-02-22 10:52:21 -0300

    fix: don't provide general file export logic

--- packages/designfile/README.md
+++ packages/designfile/README.md
@@ -4,20 +4,10 @@ Export assets from design files, we currently support Sketch, Illustrator and Fi
 
 ### Usage
 
-You can let the module figure out the correct parser for you:
-
 ```js
-import {exportSVG, canParse} from '@livedesigner/designfile';
+import {figma} from '@livedesigner/designfile/lib';
 
-if (canParse('my/design/file.ai')) {
-  await exportSVG('my/design/file.ai', 'out/folder');
+if (figma.canParse(source)) {
+  await figma.exportSVG(source, 'out/folder');
 }
-```
-
-or you can use any of the parsers individually:
-
-```js
-import {figma} from '@livedesigner/designfile/lib/exporters/figma';
-
-await figma.exportSVG('https://figma.com/file/<key>/<title>', 'out/folder');
 ```
--- packages/designfile/src/index.ts
+++ packages/designfile/src/index.ts
@@ -1,23 +1,3 @@
-import {figma} from './exporters/figma';
-import {illustrator} from './exporters/illustrator';
-import {sketch} from './exporters/sketch';
-
-const ENABLED_EXPORTERS = [figma, illustrator, sketch];
-
-const findExporter = (source: string) => {
-  return ENABLED_EXPORTERS.find((exporter) => exporter.canParse(source));
-};
-
-export const canParse = (source: string): boolean => {
-  return Boolean(findExporter(source));
-};
-
-export const exportSVG = async (source: string, out: string) => {
-  const exporter = findExporter(source);
-
-  if (exporter) {
-    return exporter.exportSVG(source, out);
-  }
-
-  return false;
-};
+export {figma} from './exporters/figma';
+export {illustrator} from './exporters/illustrator';
+export {sketch} from './exporters/sketch';
