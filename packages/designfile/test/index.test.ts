6be5280ca68165f2bcbfaa7cfc2b1984e7f3d1cd
Roberto Dip <rob@haiku.ai>
2019-02-22 11:30:02 -0300

    fix: remove obsolete test

--- packages/designfile/test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import {canParse} from '../src/';
-
-describe('#canParse', () => {
-  test('return `true` for supported parsers', () => {
-    expect(canParse('mysketch.sketch')).toBe(true);
-    expect(canParse('myilustrator.ai')).toBe(true);
-    expect(canParse('http://figma.com/file/id/')).toBe(true);
-  });
-});
