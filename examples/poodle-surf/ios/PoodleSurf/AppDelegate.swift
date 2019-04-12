6867a31fd5c926aa2e72d33102caebdc246e4222
Westin Newell <wnewell87@gmail.com>
2019-04-11 18:01:34 -0700

    feat: hide the navigation bar in PoodleSurf iOS

--- examples/poodle-surf/ios/PoodleSurf/AppDelegate.swift
+++ examples/poodle-surf/ios/PoodleSurf/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let viewController = ReportViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.isNavigationBarHidden = true
 
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = navigationController
