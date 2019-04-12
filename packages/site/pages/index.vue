9fe0ca03cccaee7302174dda67ece1e4a8e617af
Taylor Poe <taylor@haiku.ai>
2019-04-12 15:55:35 -0700

    feat: add hero & prototype sizing, spacing variables

--- packages/site/pages/index.vue
+++ packages/site/pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="page">
     <NavBar></NavBar>
-    <h1>Hello Diez, bro!</h1>
-    <p>yo thi sis my paragraph</p>
+    <div class="hero section-narrow">
+      <h1>Keep your design system in sync with your code</h1>
+      <h3>Diez is the design system compiler — for developers creating custom workflows around design systems</h3>
+      <div class="cta-holster">
+        <span class="muted">License: MIT</span>
+        <a href="#">GitHub</a>
+        <a href="#">Docs</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,4 +27,26 @@ export default class extends Vue {
 <style lang="scss" scoped>
   @import '@/assets/styles/_shared.scss';
   @import '@/assets/styles/_layout.scss';
+
+  .hero {
+    height: 80vh;
+    max-height: 1200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+  }
+
+  h1 {
+    margin-bottom: 0;
+  }
+
+  .cta-holster {
+    margin-top: $spacingXS;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: $sizingMD;
+  }
 </style>
