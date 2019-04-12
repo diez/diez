f984fbdfa9b4d8d711a444ed0c3199cd9ce8e3cc
Westin Newell <wnewell87@gmail.com>
2019-04-12 13:46:00 -0700

    fix: move UIEdgeInsets+Diez.swift into repo

---
+++ examples/poodle-surf/ios/PoodleSurf/Extensions/UIKit/UIEdgeInsets+Diez.swift
@@ -0,0 +1,16 @@
+//
+//  UIEdgeInsets+Diez.swift
+//  PoodleSurf
+//
+//  Created by Westin Newell on 4/12/19.
+//  Copyright © 2019 Haiku. All rights reserved.
+//
+
+import UIKit
+import Diez
+
+extension UIEdgeInsets {
+    init(_ insets: EdgeInsets) {
+        self.init(top: insets.top, left: insets.left, bottom: insets.bottom, right: insets.right)
+    }
+}
