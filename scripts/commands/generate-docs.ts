5282f90138d923019f630daaa8ab6215c410a804
Westin Newell <westin@haiku.ai>
2019-05-22 18:54:46 -0700

    fix(docs): allow docs to be generated when the repo is in a path with ‘src’ in it (#106)

--- scripts/commands/generate-docs.ts
+++ scripts/commands/generate-docs.ts
@@ -1,7 +1,6 @@
 import {fatalError} from '@diez/cli-core';
-import {Stats} from 'fs-extra';
-import {walkSync} from 'fs-walk';
-import {extname, join, relative, sep} from 'path';
+import glob from 'glob';
+import {dirname, join, relative} from 'path';
 import {Project} from 'ts-morph';
 import {assertNotWatching, root, run, runQuiet} from '../internal/helpers';
 
@@ -23,14 +22,11 @@ export = {
 
     const project = new Project();
 
-    walkSync(join(root, 'packages'), (basedir: string, filename: string, stats: Stats) => {
-      if (!stats.isFile() || extname(filename) !== '.ts' || !basedir.includes(`${sep}src`)) {
-        return;
-      }
-
+    const filePaths = glob.sync(join(packageRoot, '*/src/**/*.ts'));
+    for (const path of filePaths) {
       // Look for imports like:
       //   import {foo, bar} from '@diez/baz';
-      const path = join(basedir, filename);
+      const basedir = dirname(path);
       const sourceFile = project.addExistingSourceFile(path);
       for (const declaration of sourceFile.getImportDeclarations()) {
         if (!declaration.getModuleSpecifierValue().startsWith('@diez/')) {
@@ -55,7 +51,7 @@ export = {
         pathComponents[0] = relative(basedir, packageRoot);
         namespace.getNameNodes()[0].replaceWithText(`'${pathComponents.join('/')}'`);
       }
-    });
+    }
 
     await project.save();
 
