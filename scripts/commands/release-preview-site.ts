3f6c63e7159ca57257539e4e73cee4a0a46887c1
Gumptious <gumptious@haiku.ai>
2019-06-10 11:08:00 -0700

    tweak: update release*-site scripts

--- scripts/commands/release-preview-site.ts
+++ scripts/commands/release-preview-site.ts
@@ -6,7 +6,7 @@ const previewSiteRoot = join(root, 'examples', 'preview-site');
 export = {
   name: 'release-preview-site',
   description: 'Releases the Diez preview site.',
-  action: () => {
+  loadAction: () => () => {
     run('yarn build', previewSiteRoot);
     run('aws s3 sync dist s3://diez-www', previewSiteRoot);
     run(`aws cloudfront create-invalidation --distribution-id=${process.env.DIEZ_WWW_DISTRIBUTION} --paths "/*"`);
