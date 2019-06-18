a1e4ef63e0dc236ea628500f59dd09c563c5be7a
Gumptious <gumptious@haiku.ai>
2019-06-18 15:09:51 -0700

    fix(site): scope layout styles so docs do not receive extra styles (#157)

--- examples/site/docs/.vuepress/theme/layouts/Layout.vue
+++ examples/site/docs/.vuepress/theme/layouts/Layout.vue
@@ -26,50 +26,52 @@ export default {
 <style lang="scss">
   @import '@theme/styles/_utils.scss';
 
-  section {
-    margin-top: $sizingXS !important;
-    @include tablet {
-      margin-top: 0 !important;
+  .shared {
+    section {
+      margin-top: $sizingXS !important;
+      @include tablet {
+        margin-top: 0 !important;
+      }
     }
-  }
-  h1,   h2 {
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-  h3  {
-    padding-top: 68px;
-    margin-top: 0;
-  }
-  .block {
-    margin-bottom: $spacing3XL;
-  }
-  .one-quarter {
-    position: relative;
-    align-self: flex-start;
-    height: 100%;
-  }
-  .nav-wrap {
-    position: fixed;
-    top: 200;
-    a {
-      display: block;
-      margin-bottom: $spacingLG;
+    h1, h2 {
+      margin-top: 0;
+      margin-bottom: 0;
     }
-    @include tablet {
-      display: none;
+    h3  {
+      padding-top: 68px;
+      margin-top: 0;
     }
-  }
+    .block {
+      margin-bottom: $spacing3XL;
+    }
+    .one-quarter {
+      position: relative;
+      align-self: flex-start;
+      height: 100%;
+    }
+    .nav-wrap {
+      position: fixed;
+      top: 200;
+      a {
+        display: block;
+        margin-bottom: $spacingLG;
+      }
+      @include tablet {
+        display: none;
+      }
+    }
 
-  pre {
-    border-radius: $borderRadiusCard;
-    background-color: $cardColor!important;
-    box-shadow: 0 5px 0 $cardShadow, inset 0 2px 0 $cardInsetShadow;
-    margin-top: $spacing2XL!important;
-  }
+    pre {
+      border-radius: $borderRadiusCard;
+      background-color: $cardColor!important;
+      box-shadow: 0 5px 0 $cardShadow, inset 0 2px 0 $cardInsetShadow;
+      margin-top: $spacing2XL!important;
+    }
 
-  p > code {
-    border-radius: $borderRadiusCard;
-    background-color: $cardColor;
-    padding: 2px 7px;
+    p > code {
+      border-radius: $borderRadiusCard;
+      background-color: $cardColor;
+      padding: 2px 7px;
+    }
   }
 </style>
