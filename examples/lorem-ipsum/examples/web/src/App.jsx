6f0393fa9538cc8c8c0a5aa48a5d0e65546d2b5f
Roberto Dip <rob@haiku.ai>
2019-07-29 18:33:08 -0300

    tweak(lorem-ipsum): update and simplify to use hooks and remove deprecated callbacks (#219)

--- examples/lorem-ipsum/examples/web/src/App.jsx
+++ examples/lorem-ipsum/examples/web/src/App.jsx
@@ -1,63 +1,62 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { Diez, DesignSystem } from 'diez-lorem-ipsum';
 import styles from './App.module.scss';
 
-class Animation extends React.Component {
-  componentDidMount() {
-    this.props.lottie.mount(this.refs.animation);
-  }
+const Animation = (props) => {
+  const animation = useCallback((node) => {
+    if (node !== null) {
+      props.lottie.mount(node);
+    }
+  }, []);
 
-  render() {
-    return (
-      <div
-        className={styles.animation}
-        ref="animation"
-        style={this.props.style}
-      />
-    );
-  }
+  return (
+    <div
+      className={styles.animation}
+      ref={animation}
+      style={props.style}
+    />
+  );
 }
 
-export default class App extends React.PureComponent {
-  diez = new Diez(DesignSystem);
+const App = () => {
+  const [ds, setDs] = useState();
+  const diez = new Diez(DesignSystem);
 
-  componentWillMount() {
+  useEffect(() => {
     // Here we are observing hot updates to our design system.
     //
     // Since this instance of Diez was initialized with DesignSystem, it will deliver updates to the DesignSystem
     // object described in `src/DesignSystem.ts` (relative to the root of the Diez project).
-    this.diez.attach((ds) => {
-      this.setState({ ds });
-    });
+    diez.attach(setDs);
+  }, []);
+
+  if (typeof ds === 'undefined') {
+    return null;
   }
 
-  render() {
-    const { ds } = this.state;
-
-    return (
-      <div className={styles.wrapper}>
-        <div className={styles.mastheadContainer}>
-          <div className={styles.masthead} />
-        </div>
-        <div className={styles.contentContainer}>
-          <div className={styles.content}>
-            <div className={styles.icon}></div>
-            <div className={styles.innerContent}>
-              <h2 className={styles.title}>{ds.strings.title}</h2>
-              <h3 className={styles.caption}>{ds.strings.caption}</h3>
-              <div className={styles.animationContainer}>
-                <Animation
-                  lottie={ds.loadingAnimation}
-                />
-                <p className={styles.body}>{ds.strings.helper}</p>
-                <div className={styles.footer}>
-                    Refer to <a href="https://beta.diez.org/getting-started/" target="_blank">The Guides</a> for more information.
-                </div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.mastheadContainer}>
+        <div className={styles.masthead} />
+      </div>
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <div className={styles.icon}></div>
+          <div className={styles.innerContent}>
+            <h2 className={styles.title}>{ds.strings.title}</h2>
+            <h3 className={styles.caption}>{ds.strings.caption}</h3>
+            <div className={styles.animationContainer}>
+              <Animation lottie={ds.loadingAnimation} />
+              <p className={styles.body}>{ds.strings.helper}</p>
+              <div className={styles.footer}>
+                  Refer to <a href="https://beta.diez.org/getting-started/" target="_blank">The Guides</a> for more information.
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default App;
