745e7e8005536abc1b8da0347bb3f511f9946ef7
Westin Newell <westin@haiku.ai>
2019-08-01 12:57:29 -0700

    fix(targets): ignore dependency version numbers from the @diez namespace in stub's generated web SDK’s package.json for unit tests (#229)
    This alleviates failures that would occur after running the release script.

--- packages/targets/test/targets.e2e.test.ts
+++ packages/targets/test/targets.e2e.test.ts
@@ -1,4 +1,6 @@
 import {registerExpectations} from '@diez/test-utils';
+import {readJsonSync} from 'fs-extra';
+import {join} from 'path';
 import {
   buildRoot,
   createAndroidCompilerForFixture,
@@ -18,6 +20,25 @@ jest.mock('fontkit', () => ({
   openSync: () => ({postscriptName: 'SomeFont'}),
 }));
 
+/**
+ * Updates the provided package.json object so that the version number field is `-` for any `depedencies` or
+ * `devDependencies` that are prefixed with '@diez/'.
+ */
+const clearPackageJsonDiezVersions = (packageJson: any) => {
+  const diezPackagePrefix = '@diez/';
+  for (const dependency in packageJson.dependencies) {
+    if (dependency.startsWith(diezPackagePrefix)) {
+      packageJson.dependencies[dependency] = '-';
+    }
+  }
+
+  for (const dependency in packageJson.devDependencies) {
+    if (dependency.startsWith(diezPackagePrefix)) {
+      packageJson.devDependencies[dependency] = '-';
+    }
+  }
+};
+
 describe('targets.e2e', () => {
   for (const fixture of getFixtures()) {
     test(`targets.e2e.${fixture}`, async () => {
@@ -33,7 +54,21 @@ describe('targets.e2e', () => {
       const webCompiler = await createWebCompilerForFixture(fixture);
       await webCompiler.start();
 
-      expect(buildRoot).toMatchDirectory(getGoldenRoot(fixture));
+      // Ignore the web SDK's package.json in the directory comparison since version numbers may not match after a
+      // release has been created but the stub goldens have not been regenerated.
+      const packageJsonPath = join('diez-stub-web', 'package.json');
+      const blacklist = new Set([packageJsonPath]);
+
+      const goldenRoot = getGoldenRoot(fixture);
+      expect(buildRoot).toMatchDirectory(goldenRoot, blacklist);
+
+      const goldenPackageJson = readJsonSync(join(goldenRoot, packageJsonPath));
+      const builtPackageJson = readJsonSync(join(buildRoot, packageJsonPath));
+
+      clearPackageJsonDiezVersions(goldenPackageJson);
+      clearPackageJsonDiezVersions(builtPackageJson);
+
+      expect(builtPackageJson).toEqual(goldenPackageJson);
     });
   }
 });
