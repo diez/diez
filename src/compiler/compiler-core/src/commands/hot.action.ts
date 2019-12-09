2a4d5d5e168170a2e1aed091548f355c44a0c54e
Roberto Dip <rob@haiku.ai>
2019-12-09 14:31:17 -0300

    feat(cli): add a prompt to remove existing watchers (#20)

--- src/compiler/compiler-core/src/commands/hot.action.ts
+++ src/compiler/compiler-core/src/commands/hot.action.ts
@@ -1,10 +1,59 @@
 import {CliAction} from '@diez/cli-core';
 import {Target} from '@diez/engine';
-import {CompilerEvent, CompilerOptions} from '../api';
+import {prompt} from 'enquirer';
+import {removeSync} from 'fs-extra';
+import {CompilerEvent, CompilerOptions, CompilerProvider} from '../api';
 import {ProjectParser} from '../parser';
-import {getProjectRoot, getTargets, printWarnings} from '../utils';
+import {ExistingHotUrlMutexError, getProjectRoot, getTargets, printWarnings, projectCache} from '../utils';
 
-const action: CliAction = (options: CompilerOptions) => new Promise(async (resolve, reject) => {
+interface Answers {
+  removeMutex: boolean;
+}
+
+const askToRemoveHotUrl = async () => {
+  const questions: any[] = [];
+  questions.push({
+    type: 'confirm',
+    name: 'removeMutex',
+    required: true,
+    message: 'We found another hot process running, do you want to take over?.',
+  });
+
+  return await prompt<Answers>(questions);
+};
+
+const runProgram = async (options: CompilerOptions, targetProvider: CompilerProvider) =>
+  new Promise(async (resolve, reject) => {
+    const program = new ProjectParser(await getProjectRoot(), options, true);
+    // Print warnings on every `Compiled` event.
+    program.on(CompilerEvent.Compiled, () => printWarnings(program.components));
+
+    // Start the hot handler on the first `Compiled` event.
+    program.once(CompilerEvent.Compiled, () => {
+      targetProvider
+        .handler(program)
+        .catch(async (error) => {
+          if (!(error instanceof ExistingHotUrlMutexError)) {
+            return reject(error);
+          }
+
+          const {removeMutex} = await askToRemoveHotUrl();
+          if (!removeMutex) {
+            process.exit(0);
+          }
+          removeSync(error.mutexPath);
+          program.close();
+          projectCache.clear();
+          runProgram(options, targetProvider);
+
+        })
+        .then(resolve);
+    });
+
+    program.watch();
+  });
+
+const action: CliAction = async (options: CompilerOptions) => {
   options.target = options.target.toLowerCase() as Target;
   const targetProvider = (await getTargets()).get(options.target);
 
@@ -13,16 +62,7 @@ const action: CliAction = (options: CompilerOptions) => new Promise(async (resol
     throw new Error(`Invalid target: ${options.target}. See --help for options.`);
   }
 
-  const program = new ProjectParser(await getProjectRoot(), options, true);
-  // Print warnings on every `Compiled` event.
-  program.on(CompilerEvent.Compiled, () => printWarnings(program.components));
-
-  // Start the hot handler on the first `Compiled` event.
-  program.once(CompilerEvent.Compiled, () => {
-    targetProvider.handler(program).catch(reject).then(resolve);
-  });
-
-  program.watch();
-});
+  await runProgram(options, targetProvider);
+};
 
 export = action;
