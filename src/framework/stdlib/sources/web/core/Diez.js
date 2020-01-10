26eaf8bcd602c959ac2ae334ed3c5bb077ca01b0
Roberto Dip <rob@haiku.ai>
2020-01-09 21:12:20 -0300

    fix(stdlib): safeguard JavaScript SDK for server side rendering

--- src/framework/stdlib/sources/web/core/Diez.js
+++ src/framework/stdlib/sources/web/core/Diez.js
@@ -17,7 +17,12 @@ const diezHTMLExtensions = [];
 
 class Diez {
   constructor (componentType) {
-    this.iframe = document.createElement('iframe');
+    if (typeof document !== 'undefined') {
+      this.iframe = document.createElement('iframe');
+    } else {
+      this.iframe = {};
+    }
+
     this.componentType = componentType;
     this.component = new this.componentType();
     this.subscribers = [];
@@ -54,13 +59,16 @@ class Diez {
     this.iframe.width = '0';
     this.iframe.height = '0';
     this.iframe.style.display = 'none';
-    document.body.appendChild(this.iframe);
-    window.addEventListener('message', (event) => {
-      if (event.source === this.iframe.contentWindow && event.origin.startsWith(Environment.serverUrl)) {
-        this.component = new this.componentType(JSON.parse(event.data));
-        this.broadcast();
-      }
-    });
+
+    if (typeof document !== 'undefined') {
+      document.body.appendChild(this.iframe);
+      window.addEventListener('message', (event) => {
+        if (event.source === this.iframe.contentWindow && event.origin.startsWith(Environment.serverUrl)) {
+          this.component = new this.componentType(JSON.parse(event.data));
+          this.broadcast();
+        }
+      });
+    }
   }
 }
 
