32f4b7ca47e761d39e9130ff23807cecb455baa3
Roberto Dip <rob@haiku.ai>
2020-02-11 14:32:16 -0300

    fix(compiler): use web as default target and print a warning if no target is provided (#74)

--- src/compiler/compiler-core/src/commands/compile.ts
+++ src/compiler/compiler-core/src/commands/compile.ts
@@ -1,4 +1,5 @@
-import {CliCommandProvider} from '@diez/cli-core';
+import {CliCommandProvider, Log} from '@diez/cli-core';
+import {Target} from '@diez/engine';
 
 const getTargetsLazy = () => import('../utils').then(({getTargets}) => getTargets());
 
@@ -12,15 +13,16 @@ const provider: CliCommandProvider = {
       longName: 'target',
       valueName: 'target',
       description: 'The target platform for the compiled SDK.',
-      validator: async ({target}) => {
-        if (!target) {
-          throw new Error('--target is required.');
+      validator: async (options) => {
+        if (!options.target) {
+          options.target = Target.Web;
+          Log.warning(`\nWarning: please specify an explicit target using --target or -t. \nDefaulting to --target=${options.target.toString()}\n`);
         }
 
         const targets = await getTargetsLazy();
 
-        if (!targets.has(target.toString().toLowerCase())) {
-          let message = `Invalid target: ${target}. The following targets are supported:`;
+        if (!targets.has(options.target.toString().toLowerCase())) {
+          let message = `Invalid target: ${options.target}. The following targets are supported:`;
           for (const [name] of targets) {
             message += `\n - ${name}`;
           }
