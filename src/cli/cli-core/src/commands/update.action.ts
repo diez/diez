8bf616b8bfc61ad911afa87df76aa382c282f7e3
Roberto Dip <rob@haiku.ai>
2020-03-10 20:09:53 -0300

    tweak: display a message if we can't find diez packages to update

--- src/cli/cli-core/src/commands/update.action.ts
+++ src/cli/cli-core/src/commands/update.action.ts
@@ -25,20 +25,27 @@ const updateVersion = async (suggestedVersion?: string) => {
 
 const isDiezPackage = (packageName: string) => {
   return packageName === 'diez' || packageName.includes('@diez/');
-}
+};
 
 const updateAction: CliAction = async (options: UpdateOptions) => {
   const version = await updateVersion(options.toVersion);
   const updateMessage = loadingMessage(`Updating Diez and related packages to version ${version}`);
   const userPackageJson = JSON.parse(readFileSync(resolve('./package.json')).toString());
   const dependencyGroups = ['dependencies', 'devDependencies', 'optionalDependencies'];
+  const packagesToUpdate = new Set<string>();
 
   for (const dependencyGroup of dependencyGroups) {
     for (const [name] of Object.entries(userPackageJson[dependencyGroup] || {})) {
       if (isDiezPackage(name)) {
+        packagesToUpdate.add(name);
         userPackageJson[dependencyGroup][name] = `^${version}`;
       }
     }
+  }
+
+  if (!packagesToUpdate.size) {
+    Log.warning('Unable to find Diez packages to update, please try updating package.json manually or open an issue.');
+    return;
   }
 
   writeJSONSync('./package.json', userPackageJson, {spaces: 2});
@@ -47,7 +54,7 @@ const updateAction: CliAction = async (options: UpdateOptions) => {
   await packageManager.installAllDependencies();
   updateMessage.stop();
 
-  Log.info('Package installed!');
+  Log.info('✨ Diez packages updated.');
 };
 
 export = updateAction;
