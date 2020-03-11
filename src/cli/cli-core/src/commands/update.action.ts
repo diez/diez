aa56eafbf0d6879f6e8372f65c720cd36f4ee832
Roberto Dip <rob@haiku.ai>
2020-03-10 21:51:23 -0300

    tweak: simplify logic

--- src/cli/cli-core/src/commands/update.action.ts
+++ src/cli/cli-core/src/commands/update.action.ts
@@ -19,20 +19,13 @@ const updateAction: CliAction = async (options: UpdateOptions) => {
   const updateMessage = loadingMessage(`Updating Diez and related packages to version ${versionToUpdate}`);
   const userPackageJson = await readJson('./package.json');
   const dependencyGroups = ['dependencies', 'devDependencies', 'optionalDependencies'];
-  const packagesToUpdate = new Set<string>();
 
   for (const dependencyGroup of dependencyGroups) {
     for (const [name] of Object.entries(userPackageJson[dependencyGroup] || {})) {
       if (isDiezPackage(name)) {
-        packagesToUpdate.add(name);
         userPackageJson[dependencyGroup][name] = `^${versionToUpdate}`;
       }
     }
-  }
-
-  if (!packagesToUpdate.size) {
-    Log.warning('Unable to find Diez packages to update, please try updating package.json manually or open an issue.');
-    return;
   }
 
   await writeJson('./package.json', userPackageJson, {spaces: 2});
@@ -41,7 +34,7 @@ const updateAction: CliAction = async (options: UpdateOptions) => {
   await packageManager.installAllDependencies();
   updateMessage.stop();
 
-  Log.info('✨ Diez packages updated.');
+  Log.info('Diez packages updated.');
 };
 
 export = updateAction;
