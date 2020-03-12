8c0a718f61123c840497df0f5ee4f93234c9ec0a
Roberto Dip <rob@haiku.ai>
2020-03-12 19:32:59 -0300

    wip: progress on image prefab for svg support

--- src/framework/prefabs/src/image.ts
+++ src/framework/prefabs/src/image.ts
@@ -1,4 +1,5 @@
 import {prefab, Target} from '@diez/engine';
+import {existsSync} from 'fs-extra';
 import {File, FileType} from './file';
 import {Size2D} from './size2d';
 
@@ -10,8 +11,43 @@ export interface ImageData {
   file2x: File;
   file3x: File;
   file4x: File;
+  fileSvg: File;
   size: Size2D;
 }
+
+enum SupportedImageExtensions {
+  Png = 'png',
+  Svg = 'svg',
+}
+
+const getImageFileData = (src: string) => {
+  const pathComponents = src.split('/');
+  const filename = pathComponents.pop() || '';
+  const extensionLocation = filename.lastIndexOf('.');
+  const dir = pathComponents.join('/');
+  const name = filename.slice(0, extensionLocation);
+  const ext = filename.slice(extensionLocation);
+  const extensions: Record<SupportedImageExtensions, boolean> = {
+    [SupportedImageExtensions.Png]: false,
+    [SupportedImageExtensions.Svg]: false,
+  };
+
+  if (!ext) {
+    for (const format of Object.values(SupportedImageExtensions)) {
+      extensions[format] = existsSync(`${src}.${format}`);
+    }
+  }
+
+  if (ext in SupportedImageExtensions) {
+    extensions[ext as SupportedImageExtensions] = true;
+  }
+
+  return {
+    dir,
+    name,
+    extensions,
+  };
+};
 
 /**
  * Provides an abstraction for raster images. With bindings, this component can embed images in multiple platforms in
@@ -39,19 +75,30 @@ export class Image extends prefab<ImageData>() {
    * `image = Image.responsive('assets/filename.png', 640, 480);`
    */
   static responsive (src: string, width: number = 0, height: number = 0) {
-    const pathComponents = src.split('/');
-    const filename = pathComponents.pop() || '';
-    const extensionLocation = filename.lastIndexOf('.');
-    const dir = pathComponents.join('/');
-    const name = filename.slice(0, extensionLocation);
-    const ext = filename.slice(extensionLocation);
-    return new Image({
+    const {dir, name, extensions} = getImageFileData(src);
+
+    let data: Partial<ImageData> = {
       file: new File({src, type: FileType.Image}),
-      file2x: new File({src: `${dir}/${name}@2x${ext}`, type: FileType.Image}),
-      file3x: new File({src: `${dir}/${name}@3x${ext}`, type: FileType.Image}),
-      file4x: new File({src: `${dir}/${name}@4x${ext}`, type: FileType.Image}),
       size: Size2D.make(width, height),
-    });
+    };
+
+    if (extensions[SupportedImageExtensions.Png]) {
+      data = {
+        ...data,
+        file2x: new File({src: `${dir}/${name}@2x${SupportedImageExtensions.Png}`, type: FileType.Image}),
+        file3x: new File({src: `${dir}/${name}@3x${SupportedImageExtensions.Png}`, type: FileType.Image}),
+        file4x: new File({src: `${dir}/${name}@4x${SupportedImageExtensions.Png}`, type: FileType.Image}),
+      };
+    }
+
+    if (extensions[SupportedImageExtensions.Svg]) {
+      data = {
+        ...data,
+        file2x: new File({src: `${dir}/${name}${SupportedImageExtensions.Svg}`, type: FileType.Image}),
+      };
+    }
+
+    return new Image(data);
   }
 
   defaults = {
@@ -59,12 +106,16 @@ export class Image extends prefab<ImageData>() {
     file2x: new File({type: FileType.Image}),
     file3x: new File({type: FileType.Image}),
     file4x: new File({type: FileType.Image}),
+    fileSvg: new File({type: FileType.Image}),
     size: Size2D.make(0, 0),
   };
 
   options = {
     file4x: {
       targets: [Target.Android],
+    },
+    fileSvg: {
+      targets: [Target.Web],
     },
   };
 
