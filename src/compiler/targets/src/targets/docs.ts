b5eb048498ca8b650398a6e6ab154c9a5add643d
Roberto Dip <rob@haiku.ai>
2020-03-18 16:51:08 -0300

    tweak: declare  with the other built-in targets

--- src/compiler/targets/src/targets/docs.ts
+++ src/compiler/targets/src/targets/docs.ts
@@ -2,15 +2,6 @@ import {CompilerProvider} from '@diez/compiler-core';
 import {Target} from '@diez/engine/types/api';
 import {docsHandler as handler} from './docs.compiler';
 
-declare module '@diez/engine/types/api' {
-  /**
-   * Declare docs as a valid target.
-   */
-  export const enum Target {
-    Docs = 'docs',
-  }
-}
-
 const target: CompilerProvider = {
   handler,
   name: Target.Docs,
