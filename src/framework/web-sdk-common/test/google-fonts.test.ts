12a8b76252bbcda027bce773fb08ed51cd5aa3c3
Roberto Dip <rob@haiku.ai>
2020-05-15 16:22:40 -0300

    tweak: update tests

--- src/framework/web-sdk-common/test/google-fonts.test.ts
+++ src/framework/web-sdk-common/test/google-fonts.test.ts
@@ -1,13 +1,14 @@
+import {FontStyle} from '@diez/prefabs';
 import {GoogleFont, GoogleFontCollection} from '../src/google-fonts';
 
 describe('GoogleFont', () => {
   test('variations and hashes', () => {
     const font = new GoogleFont('Source Code Pro');
-    font.addVariant({style: 'normal', weight: 400});
+    font.addVariant({style: FontStyle.Normal, weight: 400});
     expect(font.hash()).toEqual('Source+Code+Pro:400normal');
 
-    font.addVariant({style: 'italic', weight: 800});
-    font.addVariant({style: 'normal', weight: 800});
+    font.addVariant({style: FontStyle.Italic, weight: 800});
+    font.addVariant({style: FontStyle.Normal, weight: 800});
     expect(font.hash()).toEqual('Source+Code+Pro:400normal,800italic,800normal');
   });
 });
@@ -15,9 +16,9 @@ describe('GoogleFont', () => {
 describe('GoogleFontCollection', () => {
   test('#url', () => {
     const collection = new GoogleFontCollection();
-    collection.set('Source Code Pro', {weight: 100, style: 'normal'});
-    collection.set('Source Code Pro', {weight: 500, style: 'italic'});
-    collection.set('Montserrat', {weight: 300, style: 'italic'});
+    collection.set('Source Code Pro', {weight: 100, style: FontStyle.Normal});
+    collection.set('Source Code Pro', {weight: 500, style: FontStyle.Italic});
+    collection.set('Montserrat', {weight: 300, style: FontStyle.Italic});
 
     expect(collection.url).toEqual(
       'https://fonts.googleapis.com/css?family=Source+Code+Pro:100normal,500italic%7CMontserrat:300italic&swap=true',
