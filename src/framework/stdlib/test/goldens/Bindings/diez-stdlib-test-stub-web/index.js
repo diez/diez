797b3b5b78186ac8428596185d24986b22371dcf
Roberto Dip <rob@haiku.ai>
2020-05-18 15:17:09 -0300

    chore: regenerate goldens

--- src/framework/stdlib/test/goldens/Bindings/diez-stdlib-test-stub-web/index.js
+++ src/framework/stdlib/test/goldens/Bindings/diez-stdlib-test-stub-web/index.js
@@ -489,10 +489,11 @@ class Typograph {
 
 module.exports.Typograph = Typograph;
 
-const {fontToCss, FontFormats, textAlignmentToCss, textDecorationsToCss, GoogleFontCollection} = require('@diez/web-sdk-common');
+const {fontToCss, FontFormats, textAlignmentToCss, textDecorationsToCss, GoogleFontCollection, GoogleFont} = require('@diez/web-sdk-common');
 
 let styleSheet;
 let cache;
+const googleFontCollection = new GoogleFontCollection();
 
 const registerFont = (font) => {
   if (!styleSheet || !cache) {
@@ -506,9 +507,8 @@ const registerFont = (font) => {
     return;
   }
 
-  if (font.file.type === 'remote') {
-    const collection = new GoogleFontCollection();
-    collection.set(font.name, {weight: font.weight, style: font.style});
+  if (GoogleFont.isGoogleFont(font)) {
+    googleFontCollection.set(font.name, {weight: font.weight, style: font.style});
     styleSheet.insertRule(`@import url(${collection.url})`);
     cache.add(font.file.src);
     return;
